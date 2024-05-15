6b48760bc1315b09b005d0a5c7842f4a170aed8e
flvsantos15 <flvsantos300@gmail.com>
2024-05-14 23:38:28 -0300

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flvsantos15/imersao17/goapi
+
+go 1.22.1
+
+require github.com/google/uuid v1.6.0
---
+++ internal/entity/entity.go
@@ -0,0 +1,34 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Category struct {
+	ID   string
+	Name string
+}
+
+func NewCategory(name string) *Category {
+	return &Category{
+		ID:   uuid.New().String(),
+		Name: name,
+	}
+}
+
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	CategoryID  string
+	ImageURL    string
+}
+
+func NewProdutc(name string, description string, price float64, categoryID, imageURL string) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+	}
+}
