3772d91bfda375921a4275585b76c8dbb8f0b952
flvsantos15 <flvsantos300@gmail.com>
2024-07-02 07:24:15 -0300

    chore: add sql file

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/imersao17")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/imersao17")
 	if err != nil {
 		panic(err.Error())
 	}
